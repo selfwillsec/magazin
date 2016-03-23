e907c7cf1f00bbe1993b977126bc5fda42fea3e1
Artemiy <selfwillsec@gmail.com>
2016-03-23 23:19:07 +0000

    added all files

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+var result = prompt("Та серьезно?", "да");
+var isAdmin = confirm("Вы - администратор?");
+
+
+if (isAdmin == true) {
+	console.log("Admin")
+} else {
+	console.log("Ne Admin")
+}
---
+++ assets/js/types.js
@@ -0,0 +1,15 @@
+var num = 123;
+var str = "";
+var bl = true;
+var nl = null;
+var undf;
+var obj = {};
+var func = function(){};
+
+console.log(typeof(num))
+console.log(typeof(str))
+console.log(typeof(bl))
+console.log(typeof(nl))
+console.log(typeof(undf))
+console.log(typeof(obj))
+console.log(typeof(func))
